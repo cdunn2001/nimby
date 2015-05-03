b56b4088e3057b8558d1b12ff2968e5ad6cf264e
Christopher Dunn <cdunn2001@gmail.com>
2015-05-03 22:00:34 +0100

    compiles

--- foo.nim
+++ foo.nim
@@ -3,9 +3,19 @@ echo "Hello World"
 import macros
 
 type
+  ValueKind = enum
+    kString, kHash, kArray, kNil
   Value = ref ValueObj
   ValueObj = object
-    v: Value
+    case kind: ValueKind
+    of kString:
+      v: string
+    of kHash:
+      vHash: string
+    of kArray:
+      vArray: string
+    of kNil:
+      nil
 
 proc `$`(v: Value): string =
   "foo"
