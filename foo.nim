e9225b873769a1d61f68a5843c12b8a0e077ea3d
Lyren Brown <lyren.brown@gmailcom>
2015-05-03 23:40:09 +0100

    parse

--- foo.nim
+++ foo.nim
@@ -1,4 +1,4 @@
-# vim: set et sw=2 ts=2:
+# vim: set et sw=2 ts=2 sts=2:
 
 echo "Hello World"
 
@@ -7,12 +7,16 @@ import tables
 
 type
   ValueKind = enum
-    kString, kHash, kArray, kNil
+    kString, kInt, kFloat, kHash, kArray, kNil
   Value = ref ValueObj
   ValueObj = object
     case kind: ValueKind
     of kString:
       vString: string
+    of kInt:
+      vInt: int
+    of kFloat:
+      vFloat: float
     of kHash:
       vHash: Table[string, Value]
     of kArray:
@@ -26,6 +30,10 @@ proc toStr(v: Value, indent: string = ""): string =
     indent & "kNil"
   of kString:
     indent & v.vString & "\n"
+  of kInt:
+    indent & $v.vInt & "\n"
+  of kFloat:
+    indent & $v.vFloat & "\n"
   of kHash:
     var s = indent & "kHash\n"
     for k, vsub in v.vHash:
@@ -44,6 +52,26 @@ var list = @[Value(vString: "value1"), Value(vString: "value2")]
 var hash = initTable[string, Value]()
 hash["foo"] = Value(vString: "bar")
 hash["lang"] = Value(vString: "nim")
+hash["verions-major"] = Value(kind: kInt, vInt: 0)
+hash["verions-minor"] = Value(kind: kInt, vInt: 1)
+hash["verions-sub"] = Value(kind: kInt, vInt: 2)
+hash["pi"] = Value(kind: kFloat, vFloat: 3.1415926)
 hash["values"] = Value(kind: kArray, vArray: list)
 var x = Value(kind: kHash, vHash: hash)
 echo($x)
+
+type
+  SliceObj = object
+
+type
+  ParserObj = object
+    
+
+proc parse_json(s: string): Value =
+    Value(kind: kNil)
+
+let json_text = readFile("example.json")
+
+var json = parse_json(json_text)
+
+echo($json)
